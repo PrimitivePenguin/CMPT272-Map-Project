0921aaf3681655823a3d492bb84cc0b1fa31a775
Camille <cna52@sfu.ca>
2024-11-26 19:36:38 -0800

    created a request table

---
+++ requests.js
@@ -0,0 +1,22 @@
+const requests = [
+    {
+        Name: 'John Doe',
+        PhoneNumber: 123456789,
+        Type: 'Medical',
+        Location: 'XYZ Dr',
+        Picture: 'IMG.url',
+        Comments: 'blah',
+        Time: 'time',
+        Status: 'Open',
+    },
+    {
+        Name: 'Jane Smith',
+        PhoneNumber: 987654321,
+        Type: 'Fire',
+        Location: 'ABC Ave',
+        Picture: 'IMG2.url',
+        Comments: 'urgent',
+        Time: 'another time',
+        Status: 'Closed',
+    }
+];
