035e479a80caa71bd9844b5d578ef3920ad4a7eb
Josevan Wijaya <wjosevansatria@gmail.com>
2024-11-30 22:31:49 -0800

    Update requests.js
    to tests sorting, function, coming later

--- requests.js
+++ requests.js
@@ -8,5 +8,28 @@ const requests = [
         Comments: 'blah',
         Time: '2024-11-26 12:00 PM',
         Status: 'Open',
+    },
+
+    {
+        Name: 'Jane Doe',
+        PhoneNumber: 1222223333,
+        Type: 'Fire',
+        Location: 'ABC Dr',
+        Picture: 'IMG.url',
+        Comments: 'bleh',
+        Time: '2024-11-27 12:00 PM',
+        Status: 'Open',
+    },
+
+    {
+        Name: 'Lisa Lisa',
+        PhoneNumber: 123456776,
+        Type: 'Accident',
+        Location: 'XXX Dr',
+        Picture: 'IMG.url',
+        Comments: 'blah',
+        Time: '2024-11-26 11:00 PM',
+        Status: 'Closed',
     }
 ];
+
